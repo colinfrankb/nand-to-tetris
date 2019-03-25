5f914bcc49a6a3890e00351c1b275e16c7e5813f
Colin <colinfrankb@gmail.com>
2019-03-25 23:24:13 +0200

    Optimized mult.asm

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -10,11 +10,9 @@
     M=0
 (LOOP) //Has a memory address in the instruction memory address space
     @R1
-    D=M
+    MD=M-1
     @END
-    D;JEQ
-    @R1
-    M=M-1
+    D;JLT
     @R0
     D=M
     @R2
@@ -24,6 +22,11 @@
 (END) 
     @END
     0;JMP
-//Has a memory address in the instruction memory address space
+// Has a memory address in the instruction memory address space
 // The infinite loop at the end is Hack's standard way to terminate
 // the execution of Hack programs
+
+// The instruction's code is mapped to addresses from 0 to n
+// The assembled binary code file will only contains lines for each
+// instruction.
+// Label symbols do not map to instructions
