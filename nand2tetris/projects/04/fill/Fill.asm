bb1fcdfcd13d6c369136fe49583d66481043868f
Colin <colinfrankb@gmail.com>
2019-03-26 23:00:56 +0200

    Implemented Fill and tested using both tests

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,20 +11,44 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Let R1 point to the pixel value (pixel = 1 is black, 0 is white)
+// Let R2 point to the screen loop counter, starting at -1
 
 (MAINLOOP)
+    @R1
+    M=0
+    @R2
+    M=-1
     @KBD
     D=M
-    @LOOP
-    D;JEQ
-
+    @BLACK
+    D;JNE
 (SCREENLOOP)
-    
-
-
+    @R2
+    MD=M+1
+    @8192
+    D=D-A
+    @MAINLOOP // Following convention of breaking out of loop first
+    D;JGE
+    @R2
+    D=M
+    @SCREEN
+    D=A+D
+    @R3
+    M=D // Place the target memory word address into a register
+    @R1
+    D=M // Load the pixel value
+    @R3
+    A=M
+    M=D
     @SCREENLOOP
-    D;JEQ
-
-    @MAINLOOP
     0;JMP
+(BLACK)
+    @R1
+    M=-1
+    @SCREENLOOP
+    0;JMP
+
+// Observations while coding functions
+// The function must appear at the end of all functionality
+// because the instructions are read one line after another
