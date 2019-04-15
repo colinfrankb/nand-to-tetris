822e8145f9bf8487037abd2c61e69d4e3fec869b
Colin <colinfrankb@gmail.com>
2019-04-15 23:48:28 +0200

    Started implementing

---
+++ nand2tetris/projects/07/VM.Net/Commands/Push.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VM.Net.Commands
+{
+    public class Push
+    {
+        public string Execute(Stack stack, string segment, int index)
+        {
+            var assemblyInstructions = new StringBuilder();
+
+            assemblyInstructions.Append(stack.PushD());
+
+            return assemblyInstructions.ToString();
+        }
+    }
+}
---
+++ nand2tetris/projects/07/VM.Net/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace VM.Net
+{
+    public class Stack
+    {
+        private int StackPointer { get; set; }
+
+        public string PushD()
+        {
+            var assemblyInstruction = new StringBuilder();
+
+            assemblyInstruction.Append($"@{StackPointer}");
+            assemblyInstruction.Append($"M=D");
+
+            return assemblyInstruction.ToString();
+        }
+
+        public void Pop(int value)
+        { }
+    }
+}
