ca9268c69d3582ffcaa8cac6162e6b475e11ad8f
Colin Bob <colin@inter8.co>
2019-04-22 23:17:35 +0200

    Implemented Function command

---
+++ nand2tetris/projects/VM.Net/Commands/SubroutineCalling/Function.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VM.Net.Commands.SubroutineCalling
+{
+    public class Function : Command
+    {
+        private readonly VMCommandsContext _context;
+        private readonly string _functionName;
+        private readonly int _k_localVariables;
+
+        public Function(VMCommandsContext context, string functionName, string k_localVariables)
+        {
+            _context = context;
+            _functionName = functionName;
+            _k_localVariables = Convert.ToInt32(k_localVariables);
+        }
+
+        public override IList<string> Execute(Stack stack)
+        {
+            var assemblyInstructions = new List<string>();
+
+            //Declare a Label Symbol for the function entry
+            assemblyInstructions.Add($"({_functionName})");
+
+            //Allocate space on the stack for local variables, initialised to 0
+            var push = new Push(_context, "constant", "0");
+            var pushAssemblyInstructions = push.Execute(stack);
+            for (int i = 0; i < _k_localVariables; i++)
+            {
+                assemblyInstructions.AddRange(pushAssemblyInstructions);
+            }
+
+            return assemblyInstructions;
+        }
+    }
+}
---
+++ nand2tetris/projects/VM.Net/Commands/SubroutineCalling/Return.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VM.Net.Commands.SubroutineCalling
+{
+    public class Return : Command
+    {
+        public override IList<string> Execute(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
